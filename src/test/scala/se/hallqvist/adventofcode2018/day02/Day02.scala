96e350f2332224f68ba138814ceb0b524d6850d1
niklas <niklas@mylifediary.com>
2018-12-03 15:14:29 +0100

    Day 3's solution

--- src/test/scala/se/hallqvist/adventofcode2018/day02/Day02.scala
+++ src/test/scala/se/hallqvist/adventofcode2018/day02/Day02.scala
@@ -27,8 +27,6 @@ Of these box IDs, four of them contain a letter which appears exactly twice, and
 
 What is the checksum for your list of box IDs?
 
-Your puzzle answer was 8892.
-
 --- Part Two ---
 Confident that your list of box IDs is complete, you're ready to find the boxes full of prototype fabric.
 
