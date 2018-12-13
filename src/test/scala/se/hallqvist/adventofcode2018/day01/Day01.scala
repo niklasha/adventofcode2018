89ec00420cff40328eadadb4bbca07b9bbeefcaa
niklas <niklas@mylifediary.com>
2018-12-13 19:21:14 +0100

    Day 13's solution

--- src/test/scala/se/hallqvist/adventofcode2018/day01/Day01.scala
+++ src/test/scala/se/hallqvist/adventofcode2018/day01/Day01.scala
@@ -31,6 +31,7 @@ Here are other example situations:
 -1, -2, -3 results in -6
 Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
 
+--- Part Two ---
 You notice that the device repeats the same frequency change list over and over. To calibrate the device, you need to find the first frequency it reaches twice.
 
 For example, using the same list of changes above, the device would loop as follows:
